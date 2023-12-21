adcb69a829858362f41c486394ee2d2169e769a2
xianxin011 <1141453778@qq.com>
2023-12-21 15:47:07 +0800

    feat: 新增关于列表处理的思考

---
+++ 前端开发总结/无依赖组件/LoadingIcon/index.tsx
@@ -0,0 +1,30 @@
+export function LoadingIcon(props: any) {
+  const { width = '200px', height = '200px', color = '#0095ff' } = props;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke={color}
+        strokeWidth="7"
+        strokeDasharray="205.271142578125 51.317785644531256"
+        d="M24.3 30C11.4 30 5 43.3 5 50s6.4 20 19.3 20c19.3 0 32.1-40 51.4-40 C88.6 30 95 43.3 95 50s-6.4 20-19.3 20C56.4 70 43.6 30 24.3 30z"
+        strokeLinecap="round"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          repeatCount="indefinite"
+          dur="1s"
+          keyTimes="0;1"
+          values="0;256.58892822265625"
+        ></animate>
+      </path>
+    </svg>
+  );
+}
